0f42be63be1eb45e13992f4b25ef6ffe7c1b03b1
beatriz <btrzmntr@gmail.com>
2018-08-01 22:52:02 -0300

    remover =

--- scripts/parse.py
+++ scripts/parse.py
@@ -75,7 +75,7 @@ def main():
         dump = makestr(parse)
         dump = dump.replace("(Token  )","")
         dump = dump.replace("(Token    )","" )
-        dump = dump.replace("=","-EQUALS-" )
+        dump = dump.replace("="," -EQUALS- " )
         print(dump)
         sys.stdout.flush()
 

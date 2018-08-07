7270161226468528dbec666564de88f664d4ab68
beatriz <btrzmntr@gmail.com>
2018-08-07 00:04:11 -0300

    teste --equal

--- scripts/parse.py
+++ scripts/parse.py
@@ -79,7 +79,7 @@ def main():
         dump = dump.replace("(Token    )","" )
         dump = dump.replace("="," -EQUALS- " )
         dump = dump.replace("_","")
-        dump = dump.replace("*", "-ALL-")
+        dump = dump.replace("*", " -ALL- ")
         #dump = dump.replace("Where where", "Where")
         #dump = dump.replace("(Where fanatical -EQUALS- '1')", "(Token where) (Identifier fanatical) (Token -EQUALS-) (Identifier '1')")
         print(dump)
